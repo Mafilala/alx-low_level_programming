21b0354cf60b9c891b4478f6319f4d2d195904be
Maruf <marufsherefa9@gmail.com>
2023-06-15 17:57:20 +0300

    task completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * main - entry point
+ *
+ * Return: 0 always
  */
 int main(void) 
 {
- char message[] = "Programming is like building a multilingual puzzle\n";
+ char message[] = "\"Programming is like building a multilingual puzzle\n";
+
  puts(message);
  return (0);
 }
