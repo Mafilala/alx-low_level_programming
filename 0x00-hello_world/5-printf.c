67481f32a8aface54d9217617f6e18aa2b22693e
Maruf <marufsherefa9@gmail.com>
2023-06-15 18:06:40 +0300

    task completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - program entry point
+ *
+ * Return: 0 no matter what
+ */
+int main(void)
+{
+printf("%s", "with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
