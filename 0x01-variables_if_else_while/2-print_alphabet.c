36baf3d62c4f8c31e6d9d1756f648520eea74024
Maruf <marufsherefa9@gmail.com>
2023-06-17 11:23:52 +0300

    task completed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Description: prints a to z
- * Returns: 0
+ * main - main block
+ * Description: prints a - to - z
+ * Return: 0
  */
-int main(void) {
+int main(void)
+{
 int i;
 
 for (i = 97; i <= 122; i++)
