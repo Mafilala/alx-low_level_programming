0c22629a1d8b896acbe28ca375fd5bb90b039318
Maruf <marufsherefa9@gmail.com>
2023-06-17 14:28:37 +0300

    task completed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints z - to - a
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 122; i >= 97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
