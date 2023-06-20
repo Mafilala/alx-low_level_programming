17bfa32ba1bb9712ad37e60569587e8c259355c5
Maruf <marufsherefa9@gmail.com>
2023-06-21 02:45:54 +0300

    task completed

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - abs function
+ * @c: An input character
+ * Description: function that do abs operation
+ * Return: returns the absolute value of a give number
+ */
+int _abs(int c)
+if (c < 0)
+c *= -1;
+return (c);
+}
