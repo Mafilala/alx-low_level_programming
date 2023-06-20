fbef2345941b69af40412419f51b0360ce612531
Maruf <marufsherefa9@gmail.com>
2023-06-21 02:25:17 +0300

    task completed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * _islower - custom funtction to check alphabet
+ * _isalpha - custom funtction to check alphabet
  * @c: An input character
  * Description: function check if a character is an alphabet or not
  * Return: 1 if is alphabet 0 otherwise
  */
 int _isalpha(int c)
 {
-	char i;
-	int alpha = 0;
+char i;
+int alpha = 0;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (i == c)
-			alpha = 1;
-	}
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i == c)
+alpha = 1;
+}
 
-  for (i = 'A'; i <= 'Z'; i++)
-	{
-		if (i == c)
-			alpha = 1;
-	}
+for (i = 'A'; i <= 'Z'; i++)
+{
+if (i == c)
+alpha = 1;
+}
 
-	return (alpha);
+return (alpha);
 }
