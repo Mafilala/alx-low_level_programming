c98ecc46916ddb356b5c88f563e052a05e7c3fca
Maruf <marufsherefa9@gmail.com>
2023-06-23 13:31:25 +0300

    task completed

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers between 0 and  9  except 2 and 4.
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 52 || i == 50) {
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int  _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char c);
+int _putchar(char c)i;
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
