8f573a62b3832bd0d8a77da3b756feeef1009327
Maruf <marufsherefa9@gmail.com>
2023-06-23 13:50:20 +0300

    task completed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,10 +7,10 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 48; num <= 57; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
-			_putchar(num);
+			_putchar((num % 10) + '0');
 	}
 
 	_putchar('\n');
