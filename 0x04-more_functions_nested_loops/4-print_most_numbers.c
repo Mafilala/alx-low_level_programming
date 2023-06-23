40fff733a2b1622cc8ba9e6ac7400b2ca02bcb67
Maruf <marufsherefa9@gmail.com>
2023-06-23 13:39:27 +0300

    task completed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,8 @@ void print_most_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i == 52 || i == 50) {
-			continue;
-		}
-		_putchar(i);
+		if (i != 52 || i != 50)
+			_putchar(i);
 	}
 	_putchar('\n');
 }
