5cb63fa10ed33b3af9c674851fd830ee4e1a61c9
Maruf <marufsherefa9@gmail.com>
2023-06-23 10:38:33 +0300

    task completed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two given inputs
+ * @c: first input i.e num1
+ * @c: second input i.e num2
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int int mul(int a, int b)
+{
+	return (a * b);
+}
