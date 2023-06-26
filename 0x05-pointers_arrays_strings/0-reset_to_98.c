ac0879fb25f9126e7321bfa59c2f519595b12f40
Maruf <marufsherefa9@gmail.com>
2023-06-26 15:31:37 +0300

    task completed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 /**
  * reset_to_98 - a function that reset any val to 98
- * @n - pointer to n
+ * @n: - pointer to n
  * Description: accepts pointer and update the value
  * Return: void
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
